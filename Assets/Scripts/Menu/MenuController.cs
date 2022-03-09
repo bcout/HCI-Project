4d271b73c8a15343c596bba676fe9c9db8205fa1
bcout <bcouturi@unb.ca>
2022-03-09 18:14:37 -0400

    Added main menu scene

---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField]
+    private Button start_button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        start_button.onClick.AddListener(StartGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
