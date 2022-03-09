2d2d56c355d2078ca614760ef5214655c6137227
bcout <bcouturi@unb.ca>
2022-03-09 15:29:30 -0400

    mouse click input is detected

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,6 +10,19 @@ public class PlayerController : MonoBehaviour
 
     private GameObject cursor;
     private Vector3 mouse_position;
+
+    // Used to initalize the event-driven input system
+    private void Awake()
+    {
+        // PlayerInputActions is an auto-generated class containing all the input actions we defined in the editor
+        PlayerInputActions player_input_actions = new PlayerInputActions();
+        
+        // They start off as disabled, so enable the player's input detection
+        player_input_actions.Enable();
+
+        // Subscribe to the "click" event. HandleClick() will be called when this input is detected.
+        player_input_actions.Player.Click.performed += HandleClick;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -31,5 +45,10 @@ public class PlayerController : MonoBehaviour
         mouse_position = Camera.main.ScreenToWorldPoint(mouse_position);
         mouse_position.z = 0;
         cursor.transform.position = new Vector3(mouse_position.x, mouse_position.y, 0);
+    }
+
+    private void HandleClick(InputAction.CallbackContext context)
+    {
+        print("Clicked!");
     }
 }
