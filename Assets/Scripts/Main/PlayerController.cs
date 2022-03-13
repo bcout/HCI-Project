3df7d7d385f0c05b25a0b5be2e6de521708bdbc3
Yash Pandya <pandya.yash297@gmail.com>
2022-03-13 17:25:06 -0300

    Gravity, sound
    - Added gravity aim assist for rounds 3,4.
    - Added sound effect so that target hit can be confirmed by the user using audio feedback
    - The target movement, quantity looked too difficult and confusing so reduced them both down a bit

--- Assets/Scripts/Main/PlayerController.cs
+++ Assets/Scripts/Main/PlayerController.cs
@@ -15,6 +15,8 @@ public class PlayerController : MonoBehaviour
     private Vector3 mouse_position;
     private bool cursor_spawned;
 
+    private AudioSource audioSource;
+
     private enum assist_mode
     {
         NONE,
@@ -42,6 +44,8 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
+        audioSource = GetComponent<AudioSource>();
+
         cursor_spawned = false;
         GameData.player_misses = 0;
 
@@ -68,7 +72,7 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
         LockCursorToGameWindow();
         if (GameData.game_state == GameData.state.RUNNING)
         {
@@ -93,9 +97,6 @@ public class PlayerController : MonoBehaviour
         mouse_position.z = 0;
         cursor.transform.position = new Vector3(mouse_position.x, mouse_position.y, 0);
 
-        /*
-         * Uncomment this to enable the aim assist modes
-         * 
         if (current_assist_mode == assist_mode.NONE || current_assist_mode == assist_mode.AREA)
         {
             // This is the normal mouse movement with no gravity assistance applied
@@ -106,9 +107,14 @@ public class PlayerController : MonoBehaviour
         }
         else if (current_assist_mode == assist_mode.GRAVITY)
         {
-            // Use gravity to assist the player's aim
+
+            int layer_mask2 = LayerMask.GetMask("Targets");
+            RaycastHit2D cc = Physics2D.CircleCast(cursor.transform.position, 0.3f, Vector2.zero, 0.5f, layer_mask2);
+            if (cc.collider != null)
+            {
+                cursor.transform.position = cc.point;
+            }
         }
-        */
     }
 
     private void HandleClick()
@@ -116,27 +122,16 @@ public class PlayerController : MonoBehaviour
         if (GameData.game_state == GameData.state.RUNNING)
         {
             int layer_mask = LayerMask.GetMask("Targets");
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, .1f, layer_mask);
-            if (hit.collider != null)
-            {
-                GameObject selected_target = hit.collider.gameObject;
-                game_controller.Score(selected_target);
-            }
-            else if (!hit)
-            {
-                game_controller.Miss();
-            }
 
-            /*
-             * Uncomment this to enable the aim assist modes
-             * 
-            if (current_assist_mode == assist_mode.NONE || current_assist_mode == assist_mode.GRAVITY)
+            if (current_assist_mode == assist_mode.NONE)
             {
                 // This does normal checking, where we check if there is an object below the cursor.
                 // It only checks a single point, and no aim assist is applied.
+
                 RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, .1f, layer_mask);
                 if (hit.collider != null)
                 {
+                    audioSource.Play(0);
                     GameObject selected_target = hit.collider.gameObject;
                     game_controller.Score(selected_target);
                 }
@@ -144,18 +139,27 @@ public class PlayerController : MonoBehaviour
                 {
                     game_controller.Miss();
                 }
+            }
+            else if (current_assist_mode == assist_mode.GRAVITY)
+            {
+                var collider = Physics2D.OverlapCircle(Camera.main.ScreenToWorldPoint(Input.mousePosition), 0.25f, layer_mask);
+                if (collider != null)
+                {
+                    audioSource.Play(0);
+                    GameObject targ = collider.gameObject;
+                    game_controller.Score(targ);
+                }
+                else if (!collider)
+                {
+                    game_controller.Miss();
+                }
 
-                //
             }
             else if (current_assist_mode == assist_mode.AREA)
             {
                 // Use the area cursor to detect when the player has selected a target
             }
-            */
         }
-
-
-
     }
 
     public void SpawnCursor()
